a33c97d1085610868b1c0645f5ef8c53f49fea09
Jonatan Macho Maza <jonamachomaza@gmail.com>
2024-10-01 11:34:57 +0200

    PATCH: Arreglos fontawesome en vite para su ruta

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -13,4 +14,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources'),
+            '@fortawesome': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free'),
+        },
+    },
 });
